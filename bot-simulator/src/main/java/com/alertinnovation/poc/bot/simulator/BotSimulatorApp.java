55cfbfcd33717ca74271d5656b6f50fc9e9d1265
avaldezintersys <avaldez@intersysconsulting.com>
2020-04-20 17:44:39 -0500

    Add spring boot dependencies

---
+++ bot-simulator/src/main/java/com/alertinnovation/poc/bot/simulator/BotSimulatorApp.java
@@ -0,0 +1,11 @@
+package com.alertinnovation.poc.bot.simulator;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BotSimulatorApp {
+    public void main(String args[]){
+        SpringApplication.run(BotSimulatorApp.class, args);
+    }
+}
---
+++ bot-simulator/src/test/java/com/alertinnovation/poc/bot/simulator/BotSimulatorAppTest.java
@@ -0,0 +1,13 @@
+package com.alertinnovation.poc.bot.simulator;
+
+import org.junit.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BotSimulatorAppTest {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
