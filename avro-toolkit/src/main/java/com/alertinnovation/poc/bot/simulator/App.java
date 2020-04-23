b4163f13a9d0c54ffa045b516438a98941fd1451
avaldezintersys <avaldez@intersysconsulting.com>
2020-04-23 10:14:54 -0500

    Generate schema first version

--- avro-toolkit/src/main/java/com/alertinnovation/poc/bot/simulator/App.java
+++ avro-toolkit/src/main/java/com/alertinnovation/poc/bot/simulator/App.java
@@ -2,6 +2,7 @@ package com.alertinnovation.poc.bot.simulator;
 
 import com.alertinnovation.poc.bot.simulator.utils.AvroUtils;
 import com.fasterxml.jackson.databind.JsonMappingException;
+import org.apache.log4j.BasicConfigurator;
 
 import java.io.FileNotFoundException;
 
@@ -9,6 +10,7 @@ public class App {
 
     public static void main(String[] args){
         try {
+            BasicConfigurator.configure();
             String schema = AvroUtils.SchemaCreation();
             AvroUtils.saveSchema(schema);
         } catch (JsonMappingException | FileNotFoundException e) {
--- avro-toolkit/src/main/java/com/alertinnovation/poc/bot/simulator/utils/AvroUtils.java
+++ avro-toolkit/src/main/java/com/alertinnovation/poc/bot/simulator/utils/AvroUtils.java
@@ -4,6 +4,8 @@ import com.alertinnovation.poc.bot.simulator.source.BotState;
 import com.fasterxml.jackson.databind.JsonMappingException;
 import com.fasterxml.jackson.dataformat.avro.AvroMapper;
 import com.fasterxml.jackson.dataformat.avro.AvroSchema;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 
 import java.io.FileNotFoundException;
 import java.io.FileOutputStream;
@@ -12,15 +14,19 @@ import java.io.PrintStream;
 
 public class AvroUtils {
 
+    private static Logger logger = LoggerFactory.getLogger( AvroUtils.class );
+
     public static String SchemaCreation() throws JsonMappingException {
         AvroMapper avroMapper = new AvroMapper();
         AvroSchema schema = avroMapper.schemaFor(BotState.class);
+        logger.info("Creating schema ...");
         return schema.getAvroSchema().toString();
     }
 
     public static void saveSchema(final String schema) throws FileNotFoundException {
-        String dir = ".";
-        OutputStream os = new FileOutputStream(dir + "/botstate.avsc");
+        String file = "./botstate.avsc";
+        logger.info("Saving schema : "+ file);
+        OutputStream os = new FileOutputStream(file);
         final PrintStream printStream = new PrintStream(os);
         printStream.println(schema);
         printStream.close();
